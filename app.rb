9ba0b7ac576ec781ed958402f6b07f7858bcfa94
jinimcoroneo <jinim1983@gmail.com>
2016-10-14 14:40:37 +0100

    removing the player global variable

--- app.rb
+++ app.rb
@@ -11,13 +11,13 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-  	$player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+  	player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
   	redirect '/play'
   end
 
   get '/play' do
-    $game = Game.new($player_1, $player_2)
   	@player_1_name = $game.player_1.name
   	@player_2_name = $game.player_2.name
     @player_1_hp = $game.player_1.hit_points
@@ -28,11 +28,10 @@ class Battle < Sinatra::Base
   get '/attack' do
     if params[:player2]
       $game.player_2_attack
-      @current_player = $game.player_1
     else
       $game.player_1_attack
-      @current_player = $game.player_2
     end
+    @current_player = $game.current_player
     erb(:attack)
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,19 @@
 class Game
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :current_player
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @current_player = player_1
   end
 
   def player_1_attack
     player_2.hit_points -= 10
+    @current_player = player_2
   end
 
   def player_2_attack
     player_1.hit_points -= 10
+    @current_player = player_1
   end
 end
